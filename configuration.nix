311e25023de15179dfce7edcf03b39740601c267
System administrator <root@nixos.home>
2017-10-01 17:20:21 +0300

    moduloidut

--- configuration.nix
+++ configuration.nix
@@ -113,7 +113,7 @@ nixpkgs.config.allowUnfree = true;
    };
  
 
-  # The NixOS release 
-   system.stateVersion = "17.03";
+  # The NixOS release to be compatible with for stateful data such as databases.
+  system.stateVersion = "17.03";
 
 }

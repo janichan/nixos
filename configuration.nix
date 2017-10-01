2f3906a4d8fb90c847df24bdcdcfc74a7b869988
Jani Changit config --global user.name Jani <janic24@hotmail.com>
2017-10-01 15:35:13 +0300

    first setup

---
+++ configuration.nix
@@ -0,0 +1,119 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+
+# Strings needs to be quoted by two single quotes, hd0,1 means sda1
+boot.loader.grub.extraEntries = ''
+ menuentry "Windows Server 2016 DATACENTER (encrypted)" { chainloader (hd0,1)+1 
+}
+'';
+
+   networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+   networking.networkmanager.enable = true;
+
+  # Select internationalisation properties.
+   i18n = {
+     consoleFont = "Lat2-Terminus16";
+     consoleKeyMap = "fi";
+     defaultLocale = "en_US.UTF-8";
+   };
+
+  # Set your time zone.
+   time.timeZone = "Europe/Helsinki";
+
+  fonts = {
+    enableFontDir = true;
+    enableCoreFonts = true;
+    enableGhostscriptFonts = true;
+    fonts = with pkgs ; [
+      liberation_ttf
+      ttf_bitstream_vera
+      dejavu_fonts
+      terminus_font
+      bakoma_ttf
+      clearlyU
+      cm_unicode
+      andagii
+      bakoma_ttf
+      inconsolata
+      gentium
+    ];
+  };
+
+nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+   environment.systemPackages = with pkgs; [
+     wget
+     vim    
+     tmux
+     firefox
+     ranger
+     neovim
+     termite
+     git
+     xfce.mousepad
+     i3
+     polybar
+     openssh
+     htop
+     rofi
+     arandr
+   ];
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+   services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable the X11 windowing system.
+   services.xserver.enable = true;
+   services.xserver.layout = "fi";
+   services.xserver.xkbOptions = "eurosign:e";
+   services.xserver.synaptics.enable = true;
+
+
+   services.xserver.displayManager.lightdm.enable = true;
+
+  # Enable the KDE Desktop Environment.
+ #   services.xserver.displayManager.sddm.enable = true;
+ #  services.xserver.desktopManager.plasma5.enable = true;
+   services.xserver.windowManager.i3.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+   users.extraUsers.jani = {
+    home = "/home/jani";
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" "docker" ];
+    uid = 1000;
+   };
+ 
+
+  # The NixOS release to be compatible with for stateful data such as databases.
+  system.stateVersion = "17.03";
+
+}
